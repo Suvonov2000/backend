abc82cf9b352985dea28c6a05be7c4a8f0ac204f
Suvonov2000 <suvonov200@gmail.com>
2025-07-20 19:12:39 +0900

    scope and IIFE Function

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,2 +1,12 @@
 require("./country_1");
 require("./country_2");
+
+(() => {
+  const countryName = "Uzbekistan";
+  console.log(countryName);
+})();
+
+(() => {
+  const countryName = "Japan";
+  console.log(countryName);
+})();
