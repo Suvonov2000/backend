7d6a50426ed52f8a54427ce33b117ce820c949dc
Suvonov2000 <suvonov200@gmail.com>
2025-07-20 19:29:21 +0900

    local module

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,5 +1,6 @@
 require("./country_1");
 require("./country_2");
+// require("./mathUtils");
 
 (() => {
   const countryName = "Uzbekistan";
@@ -10,3 +11,9 @@ require("./country_2");
   const countryName = "Japan";
   console.log(countryName);
 })();
+
+const { add, subtract, divide, multiply } = require("./mathUtils");
+console.log(add(1, 2));
+console.log(subtract(10, 5));
+console.log(divide(25, 5));
+console.log(multiply(2, 2));
