50fb78bfd61d85af02f040ef7aa09ff8bdeb9afc
Suvonov2000 <suvonov200@gmail.com>
2025-07-21 13:06:32 +0900

    __filename, __dirname, basename

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -2,3 +2,8 @@ const path = require("node:path");
 
 console.log(__dirname);
 console.log(__filename);
+
+console.log(path.basename(__filename, "js"));
+console.log(path.basename(__dirname));
+
+console.log(path.basename(__filename));
