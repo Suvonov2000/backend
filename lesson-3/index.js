72ebc6a0ca96e148fe95bbb04589a1b8f4c36321
Suvonov2000 <suvonov200@gmail.com>
2025-07-27 18:24:03 +0900

    events

---
+++ lesson-3/index.js
@@ -0,0 +1,10 @@
+const EvenEmitter = require("events");
+
+const eventEmitter = new EvenEmitter();
+
+const connectHandler = () => {
+  console.log("Successfully connected to database");
+};
+
+eventEmitter.on("connect", connectHandler);
+eventEmitter.emit("connect");
