70e04afcf02f516727f58c1519edb37a1d252724
Suvonov2000 <suvonov200@gmail.com>
2025-08-02 21:29:41 +0900

    fs read and writefile

--- ex/index.js
+++ ex/index.js
@@ -1,9 +1,20 @@
+const { error } = require("console");
 const fs = require("fs");
 const path = require("path");
 
 const file = fs.readFileSync(path.join(__dirname, "text.txt"), "utf-8");
-
-fs.writeFileSync(path.join(__dirname, "text.txt"), "Hello world", {
+fs.writeFileSync(path.join(__dirname, "text.txt"), "This is from index.js", {
   encoding: "utf-8",
 });
 console.log(file);
+
+// Bu ascynxron usulida yoilgan
+fs.readFile(path.join(__dirname, "text.txt"), (error, data) => {
+  fs.writeFile(path.join(__dirname, "text.txt"), data, "utf-8", (error) => {
+    if (error) {
+      return console.log("Failed");
+    }
+    console.log("Succeded");
+  });
+});
+console.log("Hello");
