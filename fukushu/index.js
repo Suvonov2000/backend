14f69aa99dc4a4213eeee0017d2cd21c111e7a38
Suvonov2000 <suvonov200@gmail.com>
2025-08-03 14:12:58 +0900

    fukushu

--- fukushu/index.js
+++ fukushu/index.js
@@ -6,35 +6,35 @@
 const fs = require("fs");
 const path = require("path");
 
-// const fileInfo = fs.readFileSync(path.join(__dirname, "text.txt"), "utf-8");
-// console.log(fileInfo);
+const fileInfo = fs.readFileSync(path.join(__dirname, "text.txt"), "utf-8");
+console.log(fileInfo);
 
-// fs.writeFileSync(path.join(__dirname, "test.txt"), "Salom");
-// fs.unlinkSync(path.join(__dirname, "test.txt"));
-// console.log("O'chirildi");
+fs.writeFileSync(path.join(__dirname, "test.txt"), "Salom");
+fs.unlinkSync(path.join(__dirname, "test.txt"));
+console.log("O'chirildi");
 
-// fs.appendFile(
-//   path.join(__dirname, "text.txt"),
-//   "Uzbekistan/n",
-//   "utf-8",
-//   (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//   }
-// );
-// const readableStream = fs.createReadStream(path.join(__dirname, "text.txt"), {
-//   encoding: "utf-8",
-//   highWaterMark: 2,
-// });
-// readableStream.on("end", () => {
-//   console.log("steam ended");
-// });
-
+fs.appendFile(
+  path.join(__dirname, "text.txt"),
+  "Uzbekistan/n",
+  "utf-8",
+  (err) => {
+    if (err) {
+      console.log(err);
+    }
+  }
+);
 const readableStream = fs.createReadStream(path.join(__dirname, "text.txt"), {
   encoding: "utf-8",
   highWaterMark: 2,
 });
+readableStream.on("end", () => {
+  console.log("steam ended");
+});
+
+// const readableStream = fs.createReadStream(path.join(__dirname, "text.txt"), {
+//   encoding: "utf-8",
+//   highWaterMark: 2,
+// });
 
 const writableStream = fs.createWriteStream(path.join(__dirname, "test.txt"));
 
