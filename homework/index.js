eb1c1c89efe38307ff4a65c779ddd4494da4acde
Suvonov2000 <suvonov200@gmail.com>
2025-08-04 12:40:01 +0900

    hometask

--- homework/index.js
+++ homework/index.js
@@ -1,17 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-const createFolder = (folrderNmae) => {
-  const folderPath = path.join(__dirname, folrderNmae);
-  fs.mkdirSync(folderPath);
-  console.log("Papka yaratildi");
+// const createFolder = (folrderNmae) => {
+//   const folderPath = path.join(__dirname, folrderNmae);
+//   fs.mkdirSync(folderPath);
+//   console.log("Papka yaratildi");
+// };
+
+// const createFile = (fileName) => {
+//   const filePath = path.join(__dirname, "newFolder", fileName);
+//   fs.writeFileSync(filePath, "data");
+//   console.log("file yaratildi");
+// };
+
+// createFolder("newFolder");
+// createFile("newFile.txt");
+
+const writeToFile = (content, filePath) => {
+  fs.writeFileSync(filePath, content);
+  console.log("Malumot yozildi");
+};
+writeToFile("Hello Umidjon", path.join(__dirname, "test.txt"));
+
+const writeFile = () => {
+  fs.writeFileSync(path.join(__dirname, "text.txt"), "Hello from Japan");
+  console.log("Malumot yozildi");
 };
 
-const createFile = (fileName) => {
-  const filePath = path.join(__dirname, "newFolder", fileName);
-  fs.writeFileSync(filePath, "data");
-  console.log("file yaratildi");
-};
-
-createFolder("newFolder");
-createFile("newFile.txt");
+writeFile();
